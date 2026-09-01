1b7ff5d952a4b8cb131ab2617fa39a8dbf4552b1
Matodzi <mamaleec025@student.wethinkcode.co.za>
2026-09-01 15:58:12 +0200

    Add ward record model

--- ingestion-service/src/main/java/co/wethinkcode/healthsafe/WardRecord.java
+++ ingestion-service/src/main/java/co/wethinkcode/healthsafe/WardRecord.java
@@ -1,4 +1,20 @@
 package co.wethinkcode.healthsafe;
 
 public class WardRecord {
+    public String wardId;
+    public String wing;
+    public String department;
+    public Integer bedsAvailable;
+
+    public WardRecord(
+            String wardId,
+            String wing,
+            String department,
+            Integer bedsAvailable) {
+
+        this.wardId = wardId;
+        this.wing = wing;
+        this.department = department;
+        this.bedsAvailable = bedsAvailable;
+    }
 }
